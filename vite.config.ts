3734b95f7a505354b991cc9c19db6c626a3174f7
adop <chdn786od@gmail.com>
2025-05-22 20:27:41 +0200

    update(tsconfig.json, vite.config,ts): alliases added

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@styles': path.resolve(__dirname, './src/styles'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@sections': path.resolve(__dirname, './src/sections'),
+      '@ui': path.resolve(__dirname, './src/ui'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 });
