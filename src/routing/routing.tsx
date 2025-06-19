6f2357851fb5f024345e0dc951b9a232635baa15
adop <chdn786od@gmail.com>
2025-06-20 01:34:36 +0200

    fixes

--- src/routing/routing.tsx
+++ src/routing/routing.tsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 
 export const router = createBrowserRouter([
     {
-        path: "/emotion_weather-app/",
+        path: "/emotion_weather-app",
         element: <Rootlayout />,
         // errorElement: <ErrorPage />,
         children: [
