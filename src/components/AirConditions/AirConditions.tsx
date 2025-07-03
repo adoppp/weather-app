47889d27723b6bdc39b6a86f29206f2f55c117c9
adop <chdn786od@gmail.com>
2025-07-03 22:20:20 +0200

    updates(no migration)

--- src/components/AirConditions/AirConditions.tsx
+++ src/components/AirConditions/AirConditions.tsx
@@ -4,40 +4,16 @@ import classNames from "classnames/bind";
 
 import styles from "@/components/AirConditions/AirConditions.module.scss";
 
+import { TempIcon } from "@components/AirConditions/AirConditions.hooks";
 import { CardContainer } from "@/components/CardContainer/CardContainer";
 import { weatherSelector } from "@/store/selectors/weatherSelector";
 import { useTruncNumber } from "@/utils/useTruncNumber/useTruncNumber";
 import Wind from "@assets/svg/wind.svg?react";
-import TempLow from "@assets/svg/temp_low.svg?react";
-import TempMid from "@assets/svg/temp_mid.svg?react";
-import TempUnderMid from "@assets/svg/temp_underMid.svg?react";
-import TempHigh from "@assets/svg/temp_high.svg?react";
 
 const cn = classNames.bind(styles);
 
 export const AirConditions: FC = () => {
     const currentWeatherData = useSelector(weatherSelector);
-
-    const TempIcon: FC = () => {
-        const temp = useTruncNumber(currentWeatherData?.main.feels_like);
-    
-        const numberTemp =
-            typeof temp === 'number'
-                ? temp
-                : typeof temp === 'string'
-                    ? parseFloat(temp)
-                    : 0;
-    
-        if (numberTemp > 0 && numberTemp < 10) {
-            return <TempMid className={cn("svg_temp")} />;
-        } else if (numberTemp >= 10 && numberTemp < 20) {
-            return <TempUnderMid className={cn("svg_temp")} />;
-        } else if (numberTemp > 30) {
-            return <TempHigh className={cn("svg_temp")} />
-        } else {
-            return <TempLow className={cn("svg_temp")} />;
-        }
-    };
     
     return (
         <CardContainer>
@@ -47,14 +23,14 @@ export const AirConditions: FC = () => {
                     <TempIcon />
                     <div>
                         <h4>Feels like</h4>
-                        <p>{useTruncNumber(currentWeatherData?.main.feels_like)}°</p>
+                        <p>{useTruncNumber(currentWeatherData?.current.feels_like)}°</p>
                     </div>
                 </div>
                 <div className={cn("card")}>
                     <Wind className={cn("svg_wind")} />
                     <div>
                         <h4>Wind</h4>
-                        <p>{currentWeatherData?.wind.speed} m/s</p>
+                        <p>{currentWeatherData?.current.wind_speed} m/s</p>
                     </div>
                 </div>
             </div>
---
+++ src/components/CurrentWeatherCard/CurrentWeatherCard.hooks.ts
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+
+import { weatherSelector } from "@/store/selectors/weatherSelector";
+import { useTruncNumber } from "@/utils/useTruncNumber/useTruncNumber";
+
+export const useCurrentWeatherCrad = () => {
+    const currentWeatherData = useSelector(weatherSelector);
+    const weather = currentWeatherData?.weather[0]};
+
+    const city = currentWeatherData?.name;
+    const description = weather?.description ?? "No data loaded";
+    const temp = useTruncNumber(currentWeatherData?.main.temp));
+    const iconUrl = weather?.icon
+        ? `http://openweathermap.org/img/wn/${weather.icon}@2x.png`
+        : null;
+    
+    return { city, description, temp, iconUrl };
+};
