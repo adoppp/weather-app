9417a58fb323f6789623fb86b98bdd3ad1dd1c0e
adoppp <chdn786od@gmail.com>
2025-07-15 21:36:38 +0200

    refactoring(cities slice): one geo city logic added

--- src/pages/WeatherPage/WeatherPage.hooks.tsx
+++ src/pages/WeatherPage/WeatherPage.hooks.tsx
@@ -3,22 +3,23 @@ import { useLocation } from "react-router";
 
 import { getCurrentWeather, getForecast } from "@/store/operations/weatherThunk";
 import { useGetLocation } from "@/utils/useGetLocation/useGetLocation";
-import { useSelector } from "react-redux";
 import { loaderSelector } from "@/store/selectors/loaderSelector";
 import { errorSelector } from "@/store/selectors/errorSelector";
 import { Loader } from "@/components/Loader/Loader";
 import { ErrorPage } from "@/pages/ErrorPage/ErrorPage";
 import { useLocalisation } from "@/utils/useLocalisation/useLocalisation";
 import { weatherSelector } from "@/store/selectors/weatherSelector";
-import { addCity } from "@/store/reducers/citiesSlice";
-import { useAppDispatch } from "@/store/redux.hooks";
+import { addGeoCity } from "@/store/reducers/citiesSlice";
+import { useAppDispatch, useAppSelector } from "@/store/redux.hooks";
+import { citiesSelector } from "@/store/selectors/citiesSelector";
 
 export const useWeatherPage = () => {
     const location = useGetLocation();
     const routerLocation = useLocation();
-    const weather = useSelector(weatherSelector);
-    const isLoading = useSelector(loaderSelector);
-    const error = useSelector(errorSelector);
+    const weather = useAppSelector(weatherSelector);
+    const isLoading = useAppSelector(loaderSelector);
+    const error = useAppSelector(errorSelector);
+    const cities = useAppSelector(citiesSelector);
     const { lng } = useLocalisation();
     const dispatch = useAppDispatch();
 
@@ -42,7 +43,13 @@ export const useWeatherPage = () => {
         } else if (location && location.coord) {
             dispatch(getCurrentWeather({ cords: { lat: location.coord.lat, lon: location.coord.lon }, lng }));
             dispatch(getForecast({ cords: { lat: location.coord.lat, lon: location.coord.lon }, lng }));
-            dispatch(addCity({ city: location.city?.name!, coords: { lat: location.coord.lat, lon: location.coord.lon}, index: true }));
+
+            const isSameCity = cities.find(item => item.index === true && item.city === location.city?.name);
+
+            if (!isSameCity) {
+                dispatch(addGeoCity({ city: location.city?.name!, coords: { lat: location.coord.lat, lon: location.coord.lon}, index: true }));
+            }
+            
             return;
         } else if (location && location.error) {
             const error = new Error("Choose city or turn on geo and rerload");
--- src/store/reducers/citiesSlice.ts
+++ src/store/reducers/citiesSlice.ts
@@ -29,9 +29,23 @@ const citiesSlice = createSlice({
         deleteCity(state, action: PayloadAction<string>) {
             const filtered = state.data.filter(item => item.city !== action.payload);
             state.data = filtered;
+        },
+        addGeoCity(state, action: PayloadAction<{ city: string, coords?: { lat: number, lon: number}, index: boolean}>) {
+            const indexExists = state.data.find(item => item.index === true);
+
+            if (!indexExists) {
+                state.data.unshift({ 
+                    city: action.payload.city.charAt(0).toLocaleUpperCase() + action.payload.city.slice(1), 
+                    coords: { 
+                        lat: action.payload.coords?.lat || null, 
+                        lon: action.payload.coords?.lon || null
+                    },
+                    index: action.payload.index 
+                });
+            } 
         }
     }
 });
 
-export const { addCity, deleteCity } = citiesSlice.actions;
+export const { addCity, deleteCity, addGeoCity } = citiesSlice.actions;
 export const citiesReducer = citiesSlice.reducer;
